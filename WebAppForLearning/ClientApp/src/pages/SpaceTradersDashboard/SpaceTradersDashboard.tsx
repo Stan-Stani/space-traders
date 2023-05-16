c05f19e2dbb052e260460848f75f1544f9e987bb
Stan <cyclinglinguist@gmail.com>
2023-05-15 20:04:32 -0500

    wip: work on accepting contract

--- WebAppForLearning/ClientApp/src/pages/SpaceTradersDashboard/SpaceTradersDashboard.tsx
+++ WebAppForLearning/ClientApp/src/pages/SpaceTradersDashboard/SpaceTradersDashboard.tsx
@@ -13,6 +13,7 @@ import {
   ButtonProps,
   Text,
 } from "@fluentui/react-components"
+import { clear } from "console";
 
 interface resDataState {
   createAgentResData?: object;
@@ -21,6 +22,7 @@ interface resDataState {
 }
 
 const API_USER_TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiQ1JPQURUT0FEIiwiaWF0IjoxNjgzNjYzNzIyLCJzdWIiOiJhZ2VudC10b2tlbiJ9.gltNq7auewpKPwN0ceoZPNNhjEjGuoMIAk9wzn8_o3UBAo9d-vP0KUn-5xznMmmQB3gb-9n0BxU9ux3HwQfwqIHbdU7nyCiDfTrOnpA38wNSa-CyMC6gL8S-rv7Cfp-MefzMjHjmcP4blY6pvcpP-iOLExeoBv1wPl5V6Xbd9L7aMLrPSdzlW0oNn0GMGAl7LUf4Ov9s_QOSVF_AP91fTBplquNuDOuHNSNSU0y2qgEisBbotAF1yWxEZsmYi31dWLMj34iileKU6iZGRw-I6NWL46YcF5lpMZHbBz2pNyd5GtJ33e27657owjd6uzMBIgEUKWQsddllpUXWSfxyrOeXrIgseJwPPXWdkH1s0U2Wkck_MQz3JrQZFkliOu-aQxf6TpnIS-KlI3DXsggj5oHVW44sVuXkhHGWbSwGFRw7QHUAH4SbPLtOoJt-sj1Q1FYLIZsCD3JEF4_FhDmez8QMc1RqEqORJ0kcvc2poz4bfmofJqadIAdlyc116QZ-"
+
 
 const apiUrl = "https://api.spacetraders.io/v2"
 
@@ -41,70 +43,24 @@ const useStyles = makeStyles({
   },
 })
 
-const CreateAgent = (buttonId: string) => {
-  const classes = useStyles()
 
-  const [inputText, setInputText] = useState<string>("")
-  const [resDataState, setResDataState] = useState<resDataState | null>(null)
-
-  return (
-    <>
-      <Card className={classes.root}>
-        <div className={classes.commandPanel}>
-          <Label htmlFor={inputId}>Agent Name:</Label>
-          <Input id={inputId} onChange={(e) => setInputText(e.target.value)} />
-          <Button onClick={handleClick}>Create Agent</Button>
-        </div>
-        <Text>{JSON.stringify(data)}</Text>
-      </Card>
-    </>
-  )
-}
-
-const ViewAgent = (props: InputProps) => {
-  const classes = useStyles()
-  const inputId = useId("input")
-  const [inputText, setInputText] = useState<string>("")
-  const [data, setData] = useState<any>(null)
-
-  const handleClick = () => {
-    const reqData = { symbol: inputText, faction: "COSMIC" }
-    axios
-      .post("https://api.spacetraders.io/v2/register", reqData)
-      .then((response) => {
-        console.log(response.data)
-        setResDataState((prevState) => {
-          return { ...prevState, createAgentResData: response.data }
-        })
-      })
-      .catch((error) => {
-        console.log(error)
-        // setData(error.response.data)
-      })
-  }
-
-  return (
-    <>
-      <Card className={classes.root}>
-        <div className={classes.commandPanel}>
-          <Label htmlFor={inputId}>Agent Name:</Label>
-          <Input id={inputId} onChange={(e) => setInputText(e.target.value)} />
-          <Button onClick={handleClick}>Create Agent</Button>
-        </div>
-        <Text>{JSON.stringify(data)}</Text>
-      </Card>
-    </>
-  )
-}
 
 const SpaceTradersDashboard = () => {
   const classes = useStyles()
   const inputId = useId("input")
   const createAgentButtonId = useId("createAgentButton")
   const viewAgentButtonId = useId("viewAgentButton")
+  const acceptContractButtonId = useId("acceptContractButton")
   const [inputText, setInputText] = useState<string>("")
-
   const [resDataState, setResDataState] = useState<resDataState | null>(null)
+
+  const clearError = () => { 
+    setResDataState((prevState) => ({ ...prevState, resError: undefined }))
+  }
+
+  const clearInput = () => {
+    setInputText("")
+  }
 
   const sendApiRequest = (
     urlBranch: string,
@@ -140,7 +96,8 @@ const SpaceTradersDashboard = () => {
     }
   }
 
-  const handleClick = (e) => {
+  const handleClick = (e:any) => {
+   
     
     switch (e.target.id) {
       case createAgentButtonId:
@@ -150,11 +107,14 @@ const SpaceTradersDashboard = () => {
       case viewAgentButtonId:
         sendApiRequest("my/agent", "GET")
         break
-     
-    }
 
+      case acceptContractButtonId:
+        sendApiRequest(`/my/contracts/${inputText}/accept`, "POST", { headers: { Authorization: `Bearer ${API_USER_TOKEN}` }})
+        break
+    }
     
-    
+    clearInput()
+    clearError()
   }
 
   return (
@@ -165,6 +125,7 @@ const SpaceTradersDashboard = () => {
           <Input id={inputId} onChange={(e) => setInputText(e.target.value)} />
           <Button id={createAgentButtonId} onClick={handleClick}>Create Agent</Button>
           <Button id={viewAgentButtonId} onClick={handleClick}>View Agent</Button>
+          <Button id={acceptContractButtonId} onClick={handleClick}>Accept Contract</Button>
         </div>
         <Text>{JSON.stringify(resDataState)}</Text>
       </Card>
